663f0032a711c84de3b0c8c5943965b06171ac45
desktop <haydenguyn@gmail.com>
2024-01-07 17:57:49 -0800

    Added logic to check if the programs.txt file exists and to run an information query on each line (program) if it does

--- software_inventory.sh
+++ software_inventory.sh
@@ -14,3 +14,12 @@ for program in "${software[@]}"; do
     rpm -qa | grep -i "$program" >> "programs.txt" 
 done
 
+# If programs.txt exists
+if [ -e "programs.txt" ]; then
+    # Iterate over each item in the text file and run an information query for it
+    while IFS= read -r line; do 
+        rpm -qi "$line"
+    done < "programs.txt"
+else
+    echo "progams.txt not found"
+fi
