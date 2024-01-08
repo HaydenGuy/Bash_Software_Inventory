2ec1059dc06f0a4e5f6775a4f97045aa595f19fc
desktop <haydenguyn@gmail.com>
2024-01-07 17:52:36 -0800

    Added logic to create a new text file, erase the file at the start of each time script is run, and adds the program names from the software list to the text file

--- software_inventory.sh
+++ software_inventory.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+# List of software to check
+software=("maya" "blender" "handbrake")
+
+# Deletes the content in programs.txt
+echo -n > "programs.txt"
+
+# Creates programs.txt if it doesn't exist
+touch "programs.txt"
+
+# Gets the program name for each program in software list, adds it to programs.txt
+for program in "${software[@]}"; do
+    rpm -qa | grep -i "$program" >> "programs.txt" 
+done
+
